848c3f6311972fae8c90aaeaf9012cd3a2909c68
wintrmut3 <jamesx03@gmail.com>
2021-07-12 19:48:13 -0400

    adding files

---
+++ index.js
@@ -0,0 +1,11 @@
+const { StoryFrame } = require("./StoryFrame");
+// A list of story frames, assigned to classes
+
+window.onload = function () {
+    initGame();
+};
+
+
+initGame = () => {
+    
+}
