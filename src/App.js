2a8b8db5c83ec420bf9317cc289abd677a94d01e
Jan729 <46268176+Jan729@users.noreply.github.com>
2021-07-16 19:02:31 -0400

    Remove router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import './App.css';
-import { Route, HashRouter as Router, Switch } from "react-router-dom";
+
 import {
   Header,
   Footer,
-  LandingPage,
-  StoryPage,
-  EndingPage,
-  PageNotFound
+  StoryPage
 } from "./index.js";
-
 
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Header />
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/adventure" component={StoryPage} />
-          <Route exact path="/end" component={EndingPage} />
-          <Route component={PageNotFound} />
-        </Switch>
+        <StoryPage/>
       </div>
       <Footer />
-    </Router>
+    </>
   );
 }
 
