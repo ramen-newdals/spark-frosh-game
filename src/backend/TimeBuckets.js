8db6e89b0138b5a4e35a9de90e2a784f2b7807c2
wintrmut3 <jamesx03@gmail.com>
2021-08-23 16:47:06 -0400

    Added all timebuckets

--- src/backend/TimeBuckets.js
+++ src/backend/TimeBuckets.js
@@ -1,9 +1,18 @@
 export const TimeBucket = {
   'START': 0,
-  'FROSH_WEEK': 1,
-  'CLUB_FAIR': 2,
-  'HALLOWEEN': 3,
-  'FALL_MIDTERMS': 4,
+  'FROSH_WEEK': 0,
+  'CLUB_FAIR': 0,
+  'FALL_SEMESTER': 0,
+  'HALLOWEEN': 0,
+  'FALL_EXAMS': 0,
+  'WINTER_BREAK': 0,
+  'GODIVA_WEEK':0,
+  'WINTER_SEMESTER':0,
+  'WINTER_READING_WEEK':0,
+  'WINTER_EXAMS':0,
+  'FALL_READING_WEEK':0,
+  'THANKSGIVING':0,
+  'END':0
 }
 
 
@@ -11,8 +20,17 @@ export const TimeBucketArray = [
   TimeBucket.START,
   TimeBucket.FROSH_WEEK,
   TimeBucket.CLUB_FAIR,
+  TimeBucket.THANKSGIVING,
   TimeBucket.HALLOWEEN,
-  TimeBucket.FALL_MIDTERMS
+  TimeBucket.FALL_SEMESTER,
+  TimeBucket.FALL_READING_WEEK,
+  TimeBucket.FALL_EXAMS,
+  TimeBucket.WINTER_BREAK,
+  TimeBucket.GODIVA_WEEK,
+  TimeBucket.WINTER_SEMESTER,
+  TimeBucket.WINTER_READING_WEEK,
+  TimeBucket.WINTER_EXAMS,
+  TimeBucket.END
 ]
 
 
