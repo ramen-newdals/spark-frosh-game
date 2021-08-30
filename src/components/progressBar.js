08f2cfd31d1489754eed56e901d3ed26e0c39e0a
Matt Vernooy <mattvernooy@gmail.com>
2021-08-30 19:48:47 -0400

    adding progressBar component

---
+++ src/components/progressBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const progressBar = () => {
+  
+    return (
+
+<div class="card">
+      <div class="card-header">
+        <div class="card-header-title menu-label is-light is-warning">
+        <progress class="progress is-primary" value="15" max="100">30%</progress>
+        </div>
+      </div>
+      <div class="card-content">
+        <p class="m-3">
+          
+        </p>
+      </div>
+    </div>
+
+
+
+        
+        
+        
+    );
+  };
+  
+  export default progressBar;
