7cd6ebe92324c428182fdc6d3912d3397f128c90
Janelle Law <jalaw@redhat.com>
2021-09-04 12:33:04 -0400

    Remove boid link

--- src/components/FishTankLink.js
+++ src/components/FishTankLink.js
@@ -41,10 +41,9 @@ const FishTankLink = () => {
 
   return (
     <div class="card">
-      <a href="https://master.d3fqojc7hrzjsu.amplifyapp.com/">
         <div class="card-header">
           <div class="card-header-title menu-label">
-            Check out the F!rosh Fish Tank
+            F!rosh Fish Leaderboard
           </div>
         </div>
 
@@ -53,10 +52,7 @@ const FishTankLink = () => {
           <ul class="menu-list has-text-dark is-spark-font is-size-4">
             {topFiveScores.map((froshGroup, idx) => <li>{`${idx + 1}. ${froshGroup.id}    (${froshGroup.score})`}</li>)}
           </ul>
-          <br />
-          <Image imgLink={boidPreview} />
         </div>
-      </a>
     </div>
   );
 };
